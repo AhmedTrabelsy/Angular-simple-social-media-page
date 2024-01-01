bef467d4b29b22f2ab6d68713b79deda4ceae39b
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2024-01-01 12:35:58 +0100

    implementer le liste des personnages

--- src/app/personnages/personnages.component.ts
+++ src/app/personnages/personnages.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class PersonnagesComponent {
 
+  characters = [
+    { id: '0', name: 'Laugh', image: 'assets/avataaars.png', icon: 'fa-solid fa-face-laugh', isMajor: true, followers: 44 },
+    { id: '1', name: 'Blink', image: 'assets/avataaars (1).png', icon: 'fa-solid fa-face-laugh-wink', isMajor: false, followers: 12 },
+    { id: '2', name: 'Lovely', image: 'assets/avataaars (2).png', icon: 'fa-solid fa-face-laugh-beam', isMajor: true, followers: 16 }
+  ];
+
+  followings: string[] = [];
+
+  aboonner(characterIndex: number) {
+    const character = this.characters[characterIndex];
+
+    if (!this.followings.includes(character.id)) {
+      this.followings.push(character.id);
+      character.followers++;
+    }
+  }
+
+  desabonner(characterIndex: number) {
+    const character = this.characters[characterIndex];
+    const characterIndexToRemove = this.followings.findIndex((c: any) => c.id === character.id);
+
+    if (this.followings.includes(character.id)) {
+      this.followings.splice(characterIndexToRemove, 1);
+      character.followers--;
+    }
+  }
+
+  isSubscribed(characterIndex: number): boolean {
+    const character = this.characters[characterIndex];
+
+    if (this.followings.includes(character.id)) {
+      return false;
+    }
+    return true;
+  }
 }
